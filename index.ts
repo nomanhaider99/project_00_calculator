b08134151884ad89bfc6f83f633393cd4d9ae25b
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-03-20 16:30:39 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let querries = await inquirer.prompt([
+    {type: "number", name: "num1", message: "Enter your first number: "},
+    {type: "number", name: "num2", message: "Enter your second number: "},
+    {type: "list", name: "operator", message: "Enter your operator: ", choices: ["+", "-", "*", "/"]},
+]);
+
+switch(querries.operator){
+    case "+":
+        console.log(`Answer:  ${chalk.cyan(querries.num1+querries.num2)}`);
+        break
+    case "-":
+        console.log(`Answer:  ${chalk.cyan(querries.num1-querries.num2)}`);
+        break
+    case "*":
+        console.log(`Answer:  ${chalk.cyan(querries.num1*querries.num2)}`);
+        break
+    case "/":
+        console.log(`Answer:  ${chalk.cyan(querries.num1/querries.num2)}`);
+        break
+    default: 
+        console.log("Invalid Input value!!")
+}
